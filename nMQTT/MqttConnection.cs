5b87d56575f8afddb5e661ed8ba72b41e45a16de
Mark Allanson <mark@markallanson.net>
2009-05-15 20:50:32 +0000

    Added the copyright and licence notice to some files I missed.

--- nMQTT/MqttConnection.cs
+++ nMQTT/MqttConnection.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/* 
+ * nMQTT, a .Net MQTT v3 client implementation.
+ * http://code.google.com/p/nmqtt
+ * 
+ * Copyright (c) 2009 Mark Allanson (mark@markallanson.net)
+ *
+ * Licensed under the MIT License. You may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.opensource.org/licenses/mit-license.php
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
