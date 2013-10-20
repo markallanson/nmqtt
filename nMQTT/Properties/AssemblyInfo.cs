537e09a83e8ce37e8581823ef045105982975687
Mark Allanson <mark@markallanson.net>
2013-10-20 16:05:30 +0100

    Updated the copyright messages to include "& Contributors"

--- nMQTT/Properties/AssemblyInfo.cs
+++ nMQTT/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
  * http://wiki.github.com/markallanson/nmqtt
  * http://wiki.github.com/markallanson/nmqtt
  * 
- * Copyright (c) 2009 Mark Allanson (mark@markallanson.net)
+ * Copyright (c) 2009 Mark Allanson (mark@markallanson.net) & Contributors
  *
  * Licensed under the MIT License. You may not use this file except 
  * in compliance with the License. You may obtain a copy of the License at
